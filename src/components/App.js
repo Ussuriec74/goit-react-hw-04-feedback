72e2e48f0ebc122ac3f8f81a0881a444903b5ec3
Valeriy Semko <valsemko74@gmail.com>
2022-09-04 14:11:30 +0300

    make 3 state

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,33 @@ import { Notification } from 'components/Notification/Notification';
 
 export const App = () => {
 
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const [reviews, setReviews] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
-  const  { good, neutral, bad }= reviews;
+  const reviews = {
+    good,
+    neutral,
+    bad,
+  }
 
   const feedbackIncrement = evt => {
      const label = evt.currentTarget.getAttribute('value');
-     setReviews((prevState) => {
-       return {
-         ...prevState,
-         [label]: prevState[label] + 1,
-       }
-     });
-  };
+    switch (label) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        console.log('Invalid tape')
+      }
+     };
+  
   
   const totalFeedback = () => {
     return Object.values(reviews).reduce((total, el) => total + el, 0);
